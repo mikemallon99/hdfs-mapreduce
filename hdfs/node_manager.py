b0d01bcd67acf9addd49c1126e841f7a2dc4b1f7
rkroko2 <rkroko2@illinois.edu>
2020-10-22 22:58:45 -0500

    Moved to node master class

---
+++ hdfs/node_manager.py
@@ -0,0 +1,116 @@
+from failuredetector import main as failure_detector
+import socket
+import logging
+import json
+import threading
+from enum import Enum
+
+fd_cmds = ["join", "list", "id", "leave", "fail"]
+dfs_cmds = ["start_sdfs", "master"]  # TODO == add more of these
+
+START_PORT = 12344
+
+def json_to_bytes(msg):
+    msg_string = msg.encode("UTF-8")
+    return json.dumps(msg_string)
+
+
+def bytes_to_json(b):
+    msg_string = b.decode("UTF-8")
+    return json.loads(msg_string)
+
+
+class NodeManager:
+    def __init__(self):
+        self.fd_manager = failure_detector
+        self.mem_list = failure_detector.mem_list
+        self.master_manager = None
+        self.slave_manager = None
+        self.is_slave = True
+        self.sdfs_init = False
+
+    def start_thread(self, thread_name):
+        if not thread_name:
+            logging.error("Invalid thread!")
+            return
+        if thread_name == "wait_for_sdfs_start":
+            requested_thread = threading.Thread(target=self.wait_for_sdfs_start)
+            requested_thread.start()
+
+    def process_input(self, command, arguments):
+        if command in fd_cmds:
+            if not arguments == []:
+                logging.warning("Extra arguments for command '"+command+"': "+str(arguments))
+            self.fd_manager.handle_user_input(command)
+        elif command in dfs_cmds:
+            if command == "start_sdfs":
+                if self.sdfs_init:
+                    logging.warning("SDFS already started! \n Ignoring command...\n")
+                    return
+                self.send_sdfs_start()
+                logging.info("SDFS started!")
+        else:
+            logging.warning("Unknown command entered\n")
+
+    def start_failure_detector(self, introducer_args, member_args):
+        if not introducer_args == []:
+            self.fd_manager.start_fd(introducer_args)
+        else:
+            self.fd_manager.start_fd(member_args)
+
+    def wait_for_sdfs_start(self):
+        address = (socket.gethostname(), START_PORT)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.bind(address)
+            logging.info("\nWaiting for message to start sdfs...")
+            try:
+                while not self.sdfs_init:
+                    sock.listen()
+                    connection, address = sock.accept()
+                    with connection:
+                        try:
+                            data = connection.recv(4096)
+                        except ConnectionResetError:
+                            logging.error("Client connection error")
+                        logging.info("Received request from " + str(address))
+                        if not data:
+                            logging.warning("No data received, not starting sdfs...")
+                            continue
+                        message = bytes_to_json(data)
+                        if not message['Type'] == "START_SDFS":
+                            logging.warning("Invalid message")
+                        logging.debug("Message received: " + message)
+                        # TODO == instantiate slave and begin threads
+                        # Note: the 'address' of the sender will be the master
+                        logging.info("Begin slave node setup...")
+                        ack = {'Type': "ACK"}
+                        connection.sendall(json_to_bytes(ack))
+                        self.sdfs_init = True
+            finally:
+                logging.debug("Socket closed")
+                connection.close()
+
+    def send_sdfs_start(self):
+        if not self.fd_manager.is_in_group():
+            logging.warning("Not in group, must join before starting sdfs")
+            return
+
+        node_dict = self.mem_list.get_alive_nodes_not_me(my_id=self.fd_manager.get_id())
+        for node in node_dict:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            address = (node.split(":")[0], START_PORT)
+            sock.connect(address)
+            try:
+                message = {'Type': "START_SDFS"}
+                sock.sendall(json_to_bytes(message))
+
+                while True:
+                    data = sock.recv(4096)
+                    if not data:
+                        continue
+                    else:
+                        logging.info("Ack received: " + data.decode("UTF-8"))
+                        break
+            finally:
+                logging.debug("All start messages sent, socket closed")
+                sock.close()
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import argparse
 import socket
 from enum import Enum
-from failuredetector import main as failure_detector
+from hdfs.node_manager import NodeManager
 import logging
 import threading
-# from hdfs.master_node import MasterNode
-
-sdfs_init = False
-
-fd_cmds = ["join", "list", "id", "leave", "fail"]
-dfs_cmds = ["start_sdfs", "master"]  # TODO == add more of these
 
 START_PORT = 12344
 
+node_manager = NodeManager()
+
 # TODO == Need to ensure that when a node joins the network, and sdfs is on, it will join as slave
 # TODO == Need to ensure that when a node leaves the newtwork, and sdfs is on, it will stop its sdfs
+
 
 class CommandType(Enum):
     LIST = "list"
@@ -24,90 +21,6 @@ class CommandType(Enum):
     FAIL = "fail"
     START_SDFS = "start_sdfs"
     DISP_MASTER = "master"
-
-
-def send_start_sdfs(node_list):
-    for node in node_list:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        address = (node, START_PORT)
-        sock.connect(address)
-
-        try:
-            message = "START_SDFS"  # TODO == determine the message format
-            sock.sendall(message.encode("UTF-8"))
-
-            while True:
-                data = sock.recv(4096)
-                if not data:
-                    continue
-                else:
-                    logging.info("Ack received: "+data.decode("UTF-8"))
-                    break
-        finally:
-            sock.close()
-
-
-# TODO == move this stuff to master_node.py?
-def master_thread():
-    slaves_dict = failure_detector.mem_list.get_alive_nodes_not_me(my_id=failure_detector.get_id())
-    slaves_list = []
-    print(slaves_dict)
-    for node in slaves_dict:
-        slaves_list.append(node.split(":")[0])
-    # MasterNode(nodes=slaves_list, node_ip=socket.gethostname())
-    print(slaves_list)
-    send_start_sdfs(slaves_list)
-    # TODO == begin master threads
-
-
-# TODO == move this stuff to slave_node.py?
-def wait_for_sdfs_start_thread():
-    global sdfs_init
-    address = (socket.gethostname(), START_PORT)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.bind(address)
-        logging.info("Waiting for message to start sdfs...")
-        try:
-            while not sdfs_init:
-                sock.listen()
-                connection, address = sock.accept()
-                with connection:
-                    try:
-                        data = connection.recv(4096)
-                    except ConnectionResetError:
-                        logging.error("Client connection error")
-                    logging.info("Received request from "+str(address)+": {0}".format(data))
-                    if not data:
-                        logging.warning("No data received, not starting sdfs...")
-
-                    logging.debug("Message received: "+data.decode("UTF-8"))
-                    # TODO == instantiate slave and begin threads
-                    # Note: the 'address' of the sender will be the master
-                    logging.info("Begin slave node setup...")
-                    ack = "Received"
-                    connection.sendall(ack.encode("UTF-8"))
-                    sdfs_init = True
-        finally:
-            connection.close()
-
-
-def handle_sdfs_input(cmd, arguments):
-    global sdfs_init
-    ret_msg = "Invalid Command"
-    command = CommandType(cmd)
-    if command == CommandType.START_SDFS:
-        if sdfs_init:
-            logging.warning("SDFS already started!")
-        elif failure_detector.is_in_group():
-            sdfs_init = True
-            # start master node on this machine
-            master_t = threading.Thread(target=master_thread, args=())
-            master_t.start()
-            # TODO == fd adds members using socket.gethostname by default, which is the fa20-...
-            logging.info("SDFS started!")
-        else:
-            logging.warning("Not in group, must join before starting sdfs")
-    return ret_msg
 
 
 def cmd_thread():
@@ -130,15 +43,8 @@ def cmd_thread():
         optional_args = []
         if len(split_args) > 1:
             optional_args = split_args[1:]
-
-        cmd_ret = None
-        # TODO == do something with the return messages or not?
-        if cmd in fd_cmds:
-            if not optional_args == []:
-                logging.warning("Extra arguments for command: "+cmd)
-            failure_detector.handle_user_input(cmd)
-        elif cmd in dfs_cmds:
-            handle_sdfs_input(cmd, optional_args)
+        # TODO == do something with return messages or not?
+        node_manager.process_input(cmd, optional_args)
 
 
 def parse_args():
@@ -167,15 +73,14 @@ if __name__ == '__main__':
     if args.start is True:
         # start the introducer
         introducer_args = ["--introducer"]
-        failure_detector.start_fd(introducer_args)
+        node_manager.start_failure_detector(introducer_args, [])
     else:
         # start the node as a member
         member_args = ["--introducer-host", args.host, "--introducer-port", args.port]
-        failure_detector.start_fd(member_args)
+        node_manager.start_failure_detector([], member_args)
 
-    # begin thread to listen for starting sdfs
-    start_sdfs_t = threading.Thread(target=wait_for_sdfs_start_thread)
-    start_sdfs_t.start()
+    # start a thread to listen for starting sdfs
+    node_manager.start_thread("wait_for_sdfs_start")
 
     # begin listening for commands
     cmd_thread()
