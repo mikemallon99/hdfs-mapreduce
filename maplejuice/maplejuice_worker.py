f3bce7c8ee8532505a8ef95c510c7df247143149
Robbie Krokos <rkrokos2725@gmail.com>
2020-11-19 21:58:33 -0600

    Removed worker failure handling, unnecessary

--- maplejuice/maplejuice_worker.py
+++ maplejuice/maplejuice_worker.py
@@ -16,7 +16,6 @@ class MapleJuiceWorker:
     def __init__(self, node_id):
         self.cmd_sock = None
         self.node_id = node_id
-        self.cur_phase = None
         self.fd_lock = threading.Lock()
         self.failed_node = None
         self.mjman_socket = None
@@ -76,50 +75,24 @@ class MapleJuiceWorker:
         handles all processing for the split phase of the maple command
         """
         logging.debug("Received split command: "+str(request_json))
-        self.cur_phase = "split"
         while True:
-            self.fd_lock.acquire()
-            if self.failed_node is not None:
-                # TODO == handle rebalancing
-                break
-            self.failed_node = None
-            self.fd_lock.release()
-            # TODO == actually process the split
-        self.cur_phase = None
+            # TODO == Process everything
+            continue
         return
 
     def handle_map_cmd(self, request_json):
         """
         handles all processing for the map phase of the maple command
         """
-        self.cur_phase = "map"
         logging.debug("Received map command: " + str(request_json))
-        self.cur_phase = None
         return
 
     def handle_cmbn_cmd(self, request_json):
         """
         hanldes all processing for the combine phase of the maple command
         """
-        self.cur_phase = "cmbn"
         logging.debug("Received combine command: " + str(request_json))
-        self.cur_phase = None
         return
-
-    def node_failure_callback(self, failed_node):
-        """
-        Gets called whenever a node fails
-        """
-        logging.debug("Failure detected at node "+str(failed_node))
-        if self.cur_phase is None or self.cur_phase is not "split":
-            logging.debug("Worker not in split phase, do nothing")
-            # Node failures during map/combine phase handled by master, only need to do something during split
-            return
-        else:
-            # Acquire lock to notify worker that a node has failed and rebalancing is needed
-            self.fd_lock.acquire()
-            self.failed_node = failed_node
-            self.fd_lock.release()
 
     def send_maplejuice_msg(self, master_addr, msg_json):
         """
