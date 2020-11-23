ccd4ff86f12085385a4a88b8e2169f75bfa90077
Mikey <mikey@Nicks-Mac-mini.local>
2020-11-22 21:22:58 -0600

    filename

--- maplejuice/maplejuice_worker.py
+++ maplejuice/maplejuice_worker.py
@@ -398,7 +398,7 @@ def run_maple_exe(maple_exe, src_file):
     The return of the maple executable should be in the format [(k1, v1), (k2, v2), ...]
     """
     module_name = maple_exe.split(".")[0]
-    __import__('hdfs_files/' + module_name)
+    __import__('hdfs_files.' + module_name)
     maple_module = sys.modules[module_name]
     key = get_key_from_in_filename(src_file)
     with open(src_file, "r") as src_fp:
