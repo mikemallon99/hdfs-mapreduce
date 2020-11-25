d47c1327e6725c9905e0635b86da6c34821089e5
Mikey <mikey@Nicks-Mac-mini.local>
2020-11-24 21:35:47 -0600

    map reduce funcs

--- maplejuiceapps/uiucmaple1.py
+++ maplejuiceapps/uiucmaple1.py
@@ -2,7 +2,11 @@
 def maple(key_value_pairs):
     output_pairs = []
     for key, values in key_value_pairs:
-        if int(values) > 30:
+        try:
+            units = int(values)
+        except ValueError:
+            units = 0
+        if units > 30:
             output_pairs.append([1, values])
     return output_pairs
 
@@ -10,7 +14,7 @@ def map_format(string_list):
     output_lines = []
     for string in string_list:
         key = string.split(",")[0]
-        units = string.split(",")[3].replace(" ", "")
+        units = string.split(",").get(3, "").replace(" ", "")
         if units == "":
             continue
         output_lines.append([key, units])
