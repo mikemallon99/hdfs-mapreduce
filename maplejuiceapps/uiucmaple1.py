6eeabba3d6c054d0c4b2b3267bbe7834036597be
Mikey <mikey@Nicks-Mac-mini.local>
2020-11-24 21:39:36 -0600

    map reduce funcs

--- maplejuiceapps/uiucmaple1.py
+++ maplejuiceapps/uiucmaple1.py
@@ -10,11 +10,17 @@ def maple(key_value_pairs):
             output_pairs.append([1, values])
     return output_pairs
 
+def safe_list_get(l, idx, default):
+  try:
+    return l[idx]
+  except IndexError:
+    return default
+
 def map_format(string_list):
     output_lines = []
     for string in string_list:
         key = string.split(",")[0]
-        units = string.split(",").get(3, "").replace(" ", "")
+        units = safe_list_get(string.split(","), 3, "").replace(" ", "")
         if units == "":
             continue
         output_lines.append([key, units])
